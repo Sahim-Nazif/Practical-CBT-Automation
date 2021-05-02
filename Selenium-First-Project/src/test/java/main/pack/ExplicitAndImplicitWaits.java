afebb9be8b90d8494e5bc1c40648e00dc0212a4e
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-02 18:59:47 -0400

    created web driver wait instance

--- Selenium-First-Project/src/test/java/main/pack/ExplicitAndImplicitWaits.java
+++ Selenium-First-Project/src/test/java/main/pack/ExplicitAndImplicitWaits.java
@@ -1,7 +1,13 @@
 package main.pack;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -14,10 +20,31 @@ public class ExplicitAndImplicitWaits {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 
-		String URL = "";
+		String URL = "https://www.pexels.com/";
 
 		driver.get(URL);
+		driver.manage().window().maximize();
 
+		/*
+		 * Explicit wait Explicit wait is used to tell the Web Driver to wait for
+		 * certain conditions (Expected Conditions) or maximum time exceeded before
+		 * throwing "ElementNotVisibleException" exception
+		 */
+
+		// create an instance of explicit wait
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		WebElement searchBar = driver.findElement(By.xpath("//input[@id='search']"));
+		searchBar.sendKeys("Cherry Blossom");
+		searchBar.sendKeys(Keys.RETURN);
+
+		endSession();
+
+	}
+
+	public static void endSession() {
+
+		driver.close();
 	}
 
 }
