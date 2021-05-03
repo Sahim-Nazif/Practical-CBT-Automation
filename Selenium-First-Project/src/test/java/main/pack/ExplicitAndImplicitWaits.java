13fd294510eebae0b56a9a1bf802d56b2f097c9f
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-02 21:04:54 -0400

    implicit wait added & tested

--- Selenium-First-Project/src/test/java/main/pack/ExplicitAndImplicitWaits.java
+++ Selenium-First-Project/src/test/java/main/pack/ExplicitAndImplicitWaits.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -27,21 +25,32 @@ public class ExplicitAndImplicitWaits {
 		driver.manage().window().maximize();
 
 		/*
-		 * Explicit wait Explicit wait is used to tell the Web Driver to wait for
-		 * certain conditions (Expected Conditions) or maximum time exceeded before
-		 * throwing "ElementNotVisibleException" exception
+		 * Explicit wait is used to tell the Web Driver to wait for certain conditions
+		 * (Expected Conditions) or maximum time exceeded before throwing
+		 * "ElementNotVisibleException" exception
+		 */
+		// create an instance of explicit wait
+		// WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		/*
+		 * Implicit wait: is used to tell the web driver to wait for a certain amount of
+		 * time before it throws a "No Such Element Exception The default setting is 0
 		 */
 
-		// create an instance of explicit wait
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 		WebElement searchBar = driver.findElement(By.xpath("//input[@name='q']"));
 		searchBar.sendKeys("Cherry Blossom");
 		searchBar.sendKeys(Keys.RETURN);
 
-		WebElement searchElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By
-				.xpath("/html//ol[@id='b_results']/li[3]/h2/a[@href='https://en.wikipedia.org/wiki/Cherry_blossom']")));
-		System.out.println(searchElement.getText());
+		// WebElement searchElement =
+		// wait.until(ExpectedConditions.visibilityOfElementLocated(By
+		// .xpath("/html//ol[@id='b_results']/li[3]/h2/a[@href='https://en.wikipedia.org/wiki/Cherry_blossom']")));
+		// System.out.println(searchElement.getText());
+
+		WebElement searchElement = driver.findElement(By
+				.xpath("/html//ol[@id='b_results']/li[3]/h2/a[@href='https://en.wikipedia.org/wiki/Cherry_blossom']"));
+		searchElement.click();
 
 		endSession();
 
