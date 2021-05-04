9d287c021d3dbc392e28aa8ed651407364257b60
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-04 14:10:08 -0400

    get number of rows in table

--- Selenium-First-Project/src/test/java/main/pack/WebTable.java
+++ Selenium-First-Project/src/test/java/main/pack/WebTable.java
@@ -1,6 +1,10 @@
 package main.pack;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -15,11 +19,16 @@ public class WebTable {
 
 		driver = new ChromeDriver();
 
-		String URL = "";
+		String URL = "https://www.selenium.dev/downloads/";
 
 		driver.manage().window().maximize();
 
 		driver.get(URL);
+
+		// setup 1 Find number of rows in the table
+		List<WebElement> tbRows = driver.findElements(By.xpath("//table[@class='data-list']/tbody/tr"));
+
+		System.out.println(tbRows.size());
 
 	}
 
