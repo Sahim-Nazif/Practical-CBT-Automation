9cbfd1eab9672541f2485532a83293999522f1b7
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-04 14:53:09 -0400

    retrieve all table data

--- Selenium-First-Project/src/test/java/main/pack/WebTable.java
+++ Selenium-First-Project/src/test/java/main/pack/WebTable.java
@@ -1,7 +1,5 @@
 package main.pack;
 
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -26,18 +24,27 @@ public class WebTable {
 		driver.get(URL);
 
 		// step 1 Find number of Rows in the table
-		List<WebElement> tbRows = driver.findElements(By.xpath("//table[@class='data-list']/tbody/tr"));
+		int tbRows = driver.findElements(By.xpath("//table[@class='data-list']/tbody/tr")).size();
 
-		System.out.println(tbRows.size());
+		System.out.println(tbRows);
 		// step 2 Find number of Columns in the table
-		List<WebElement> tbCols = driver.findElements(By.xpath("//table[@class='data-list']/thead/tr/th"));
+		int tbCols = driver.findElements(By.xpath("//table[@class='data-list']/thead/tr/th")).size();
 
-		System.out.println(tbCols.size());
+		System.out.println(tbCols);
 
 		// step 3 Retrieve the specific row/column data
 		WebElement value = driver.findElement(By.xpath("//table[@class='data-list']//tr[2]/td[1]"));
 		System.out.println(value.getText());
 
+		// step 4 Retrieve all the rows and columns
+
+		for (int r = 1; r <= tbRows; r++) {
+			for (int c = 1; c <= tbCols; c++) {
+				WebElement tbData = driver
+						.findElement(By.xpath("//table[@class='data-list']//tr[" + r + "]/td[" + c + "]"));
+				System.out.println(tbData.getText());
+			}
+		}
 		closeBrowser();
 
 	}
