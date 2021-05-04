5f293bb1c4a3a03d7d98de271139ff0432f1581f
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-04 16:43:46 -0400

    created a new class and setup webdriver

---
+++ Selenium-First-Project/src/test/java/main/pack/DynamicWebTable.java
@@ -0,0 +1,23 @@
+package main.pack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DynamicWebTable {
+
+	private static WebDriver driver;
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+
+		String URL = "https://demo.opencart.com/admin/index.php?route=common/login";
+		driver.get(URL);
+
+	}
+
+}
