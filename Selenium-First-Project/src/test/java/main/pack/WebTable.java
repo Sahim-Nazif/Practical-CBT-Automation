4fd551b943ba2ca914224bc2055832b30e256e01
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-04 13:52:14 -0400

    webdriver manager instance created

--- Selenium-First-Project/src/test/java/main/pack/WebTable.java
+++ Selenium-First-Project/src/test/java/main/pack/WebTable.java
@@ -1,12 +1,25 @@
 package main.pack;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class WebTable {
 
 	private static WebDriver driver;
 
 	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+
+		String URL = "";
+
+		driver.manage().window().maximize();
+
+		driver.get(URL);
 
 	}
 
