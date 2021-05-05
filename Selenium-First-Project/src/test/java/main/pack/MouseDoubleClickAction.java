15996b50612d57e93ac1f9373ed0ae2f32736dbf
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-05 13:09:28 -0400

    button webelement retrieved

--- Selenium-First-Project/src/test/java/main/pack/MouseDoubleClickAction.java
+++ Selenium-First-Project/src/test/java/main/pack/MouseDoubleClickAction.java
@@ -2,8 +2,11 @@ package main.pack;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -17,11 +20,16 @@ public class MouseDoubleClickAction {
 
 		driver = new ChromeDriver();
 
-		String URL = "http://swisnl.github.io/jQuery-contextMenu/demo.html";
+		String URL = "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick";
 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get(URL);
+
+		WebElement btn = driver.findElement(By.xpath("//button[normalize-space()='Double-click me']"));
+		Actions action = new Actions(driver);
+
+		action.doubleClick(btn).perform();
 
 	}
 
