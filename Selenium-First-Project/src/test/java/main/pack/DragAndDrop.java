889fa5fda1d3c3da408adc13063a4b09fd6d696a
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-05 13:27:43 -0400

    webdriver instance created

--- Selenium-First-Project/src/test/java/main/pack/DragAndDrop.java
+++ Selenium-First-Project/src/test/java/main/pack/DragAndDrop.java
@@ -1,10 +1,27 @@
 package main.pack;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class DragAndDrop {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static WebDriver driver;
 
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+
+		String URL = "https://www.w3schools.com/html/tryit.asp?filename=tryhtml5_draganddrop";
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get(URL);
 	}
 
 }
