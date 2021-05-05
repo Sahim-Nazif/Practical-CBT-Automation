a48cd80c1b09fb4a85b9b3a3dbb48e0765ce71af
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-05 14:00:04 -0400

    drag and drop tested with different website

--- Selenium-First-Project/src/test/java/main/pack/MouseDragAndDropAction.java
+++ Selenium-First-Project/src/test/java/main/pack/MouseDragAndDropAction.java
@@ -2,8 +2,11 @@ package main.pack;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -17,13 +20,20 @@ public class MouseDragAndDropAction {
 
 		driver = new ChromeDriver();
 
-		String URL = "https://www.w3schools.com/html/tryit.asp?filename=tryhtml5_draganddrop";
+		String URL = "http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html";
 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get(URL);
 
-		driver.switchTo().frame("iframeResult");
+		// element to drag
+		WebElement sourceCity = driver.findElement(By.id("box3"));
+		// place
+		WebElement targetCountry = driver.findElement(By.id("box103"));
+
+		Actions action = new Actions(driver);
+
+		action.dragAndDrop(sourceCity, targetCountry).perform();
 
 		delayDisplay(3000);
 
