e19f4de3ebc731b441fc26f0ee12004488548913
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-05 11:03:47 -0400

    click on arrow to get matched date

--- Selenium-First-Project/src/test/java/main/pack/DatePickingAutomated.java
+++ Selenium-First-Project/src/test/java/main/pack/DatePickingAutomated.java
@@ -2,7 +2,9 @@ package main.pack;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -15,12 +17,51 @@ public class DatePickingAutomated {
 
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		String URL = "";
+
+		String URL = "https://www.cheapflights.ca/";
 		driver.manage().window().maximize();
 
 		driver.get(URL);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		String year = "2021";
+		String month = "August";
+		String date = "11";
+		WebElement departDate = driver.findElement(By.xpath("//div[@class='date-input-inner-text _j0H']"));
+		departDate.click();
+		// departDate.sendKeys(year, month, date);
 
+		while (true) {
+			String monthYear = driver.findElement(By.xpath("//div[normalize-space()='May 2021']")).getText();
+
+			String array[] = monthYear.split(" ");
+			String mon = array[0];
+			String yr = array[1];
+
+			if (mon.equalsIgnoreCase(month) && yr.equalsIgnoreCase(year))
+				break;
+
+			else
+				driver.findElement(By.xpath("//div[@id='stl-jam-cal-nextMonth']//*[local-name()='svg']")).click();
+
+		}
+
+		delayDisplay(3000);
+		closeBrowser();
+
+	}
+
+	public static void closeBrowser() {
+		driver.close();
+	}
+
+	public static void delayDisplay(long seconds) {
+
+		try {
+			Thread.sleep(seconds);
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+		}
 	}
 
 }
