4c51e8ec2629b0d07be73a264f67cc4824b4975e
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-05 14:55:46 -0400

    input box webelement retrieved and character sent

--- Selenium-First-Project/src/test/java/main/pack/KeyBoardAction.java
+++ Selenium-First-Project/src/test/java/main/pack/KeyBoardAction.java
@@ -2,8 +2,12 @@ package main.pack;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -22,6 +26,14 @@ public class KeyBoardAction {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get(URL);
+
+		WebElement input = driver.findElement(By.xpath("//input[@id='target']"));
+
+		Actions action = new Actions(driver);
+
+		action.sendKeys(input, "L").perform();
+
+		action.sendKeys(Keys.ENTER).perform();
 
 	}
 
