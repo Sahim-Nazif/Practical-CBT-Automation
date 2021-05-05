65d66ba0905aa3d3224581937a5d0d7d9b8c3e33
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-05 19:11:30 -0400

    get url and open link on new tab

--- Selenium-First-Project/src/test/java/main/pack/OpenLinkNewPage.java
+++ Selenium-First-Project/src/test/java/main/pack/OpenLinkNewPage.java
@@ -2,6 +2,8 @@ package main.pack;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -16,11 +18,15 @@ public class OpenLinkNewPage {
 
 		driver = new ChromeDriver();
 
-		String URL = "";
+		String URL = "https://www.pexels.com/";
 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get(URL);
+
+		String tab = Keys.chord(Keys.CONTROL, Keys.RETURN);
+
+		driver.findElement(By.linkText("Explore")).sendKeys(tab);
 	}
 
 }
