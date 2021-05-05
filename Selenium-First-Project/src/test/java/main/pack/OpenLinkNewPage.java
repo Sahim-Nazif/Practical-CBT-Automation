d3a9e713ae886af32279788cba70dc815f5cf77c
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-05 17:30:32 -0400

    open link in new page class created

---
+++ Selenium-First-Project/src/test/java/main/pack/OpenLinkNewPage.java
@@ -0,0 +1,26 @@
+package main.pack;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OpenLinkNewPage {
+
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+
+		String URL = "";
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get(URL);
+	}
+
+}
