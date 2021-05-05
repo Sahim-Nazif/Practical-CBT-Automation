b41e57d67f489c635df1666fd41392bb9eb95d6f
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-05 14:29:29 -0400

    thread sleep and close browser methods

--- Selenium-First-Project/src/test/java/main/pack/MouseHoverAction.java
+++ Selenium-First-Project/src/test/java/main/pack/MouseHoverAction.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -28,5 +29,28 @@ public class MouseHoverAction {
 		WebElement menu2 = driver.findElement(By.xpath("// a[normalize-space()='Main Item 2']"));
 		WebElement subItem = driver.findElement(By.xpath("// ul[@id='nav']/li[2]/ul/li[1]/a[@href='#']"));
 
+		Actions action = new Actions(driver);
+
+		action.moveToElement(menu2).moveToElement(subItem).perform();
+
+		delayDisplay(3000);
+
+		closeBrowser();
+
+	}
+
+	public static void delayDisplay(long miliseconds) {
+
+		try {
+			Thread.sleep(miliseconds);
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void closeBrowser() {
+		driver.close();
 	}
 }
