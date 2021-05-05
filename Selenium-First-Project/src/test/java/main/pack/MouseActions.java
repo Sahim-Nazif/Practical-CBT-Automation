a0bb196b68c9a542802c06238d8a0176b5f73457
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-05 12:32:15 -0400

    web driver setup

--- Selenium-First-Project/src/test/java/main/pack/MouseActions.java
+++ Selenium-First-Project/src/test/java/main/pack/MouseActions.java
@@ -1,9 +1,27 @@
 package main.pack;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class MouseActions {
 
+	private static WebDriver driver;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+
+		String URL = "https://www.bing.com/";
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get(URL);
 
 	}
 
