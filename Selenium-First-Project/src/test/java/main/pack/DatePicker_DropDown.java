364cbac55cea0764569f2e2766b882dbad82b017
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-05 11:50:30 -0400

    create webdriver instance

--- Selenium-First-Project/src/test/java/main/pack/DatePicker_DropDown.java
+++ Selenium-First-Project/src/test/java/main/pack/DatePicker_DropDown.java
@@ -1,8 +1,26 @@
 package main.pack;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class DatePicker_DropDown {
 
+	private static WebDriver driver;
+
 	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+
+		String URL = "https://www.dummyticket.com/dummy-ticket-for-visa-application/";
+		driver.manage().window().maximize();
+
+		driver.get(URL);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 	}
 
