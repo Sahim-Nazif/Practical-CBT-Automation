a1399c057e69c1f4fa5aef463b332ec6200a8da3
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-05 14:25:01 -0400

    webelements retrieved

--- Selenium-First-Project/src/test/java/main/pack/MouseHoverAction.java
+++ Selenium-First-Project/src/test/java/main/pack/MouseHoverAction.java
@@ -2,7 +2,9 @@ package main.pack;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -17,10 +19,14 @@ public class MouseHoverAction {
 
 		driver = new ChromeDriver();
 
-		String URL = "";
+		String URL = "https://demoqa.com/menu/#";
 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get(URL);
+
+		WebElement menu2 = driver.findElement(By.xpath("// a[normalize-space()='Main Item 2']"));
+		WebElement subItem = driver.findElement(By.xpath("// ul[@id='nav']/li[2]/ul/li[1]/a[@href='#']"));
+
 	}
 }
