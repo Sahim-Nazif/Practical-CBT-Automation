3124807b39c3f6f2fcc7adfaddb2023254be3935
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-05 15:31:59 -0400

    send text and select text via keyboard

--- Selenium-First-Project/src/test/java/main/pack/MultipleKeyBoardAction.java
+++ Selenium-First-Project/src/test/java/main/pack/MultipleKeyBoardAction.java
@@ -3,9 +3,11 @@ package main.pack;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -27,6 +29,16 @@ public class MultipleKeyBoardAction {
 
 		WebElement txtarea1 = driver.findElement(By.xpath("//textarea[@id='inputText1']"));
 		WebElement txtarea2 = driver.findElement(By.xpath("//textarea[@id='inputText2']"));
+
+		txtarea1.sendKeys("Automation Developer");
+
+		Actions action = new Actions(driver);
+
+		// CTRL + A
+		action.keyDown(Keys.CONTROL);
+		action.sendKeys("a");
+		action.keyUp(Keys.CONTROL);
+		action.perform();
 
 	}
 
