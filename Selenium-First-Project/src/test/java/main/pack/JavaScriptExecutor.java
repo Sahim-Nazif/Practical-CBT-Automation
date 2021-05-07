0418a9b64c5df0d7ae3d7d56c40a8d00b7b2bede
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-07 16:41:52 -0400

    send alert using js-executor

--- Selenium-First-Project/src/test/java/main/pack/JavaScriptExecutor.java
+++ Selenium-First-Project/src/test/java/main/pack/JavaScriptExecutor.java
@@ -1,5 +1,8 @@
 package main.pack;
 
+import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -14,9 +17,15 @@ public class JavaScriptExecutor {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 
-		String URL = "";
-
+		String URL = "https://www.google.com/";
+		driver.manage().window().maximize();
 		driver.get(URL);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		// using JavaScriptExecutor
+		// send alert
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("alert('google chrome is opened')");
 
 	}
 
