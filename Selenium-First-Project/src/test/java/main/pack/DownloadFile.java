2be53ce02ee432b5c96c6634adeda5b69389e524
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-07 10:36:36 -0400

    URL and xpath locator updated

--- Selenium-First-Project/src/test/java/main/pack/DownloadFile.java
+++ Selenium-First-Project/src/test/java/main/pack/DownloadFile.java
@@ -18,14 +18,13 @@ public class DownloadFile {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 
-		String URL = "https://file-examples.com/index.php/sample-documents-download/";
+		String URL = "https://file-examples.com/index.php/sample-documents-download/sample-doc-download/";
 		driver.manage().window().maximize();
 
 		driver.get(URL);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-		WebElement btn = driver.findElement(By.xpath(
-				"//a[@href='https://file-examples.com/index.php/sample-documents-download/sample-doc-download/']"));
+		WebElement btn = driver.findElement(By.xpath("//tbody/tr[1]/td[5]/a[1]"));
 		System.out.println(btn.getAttribute("href"));
 		System.out.println(btn.getText());
 		// download the MS Word file
