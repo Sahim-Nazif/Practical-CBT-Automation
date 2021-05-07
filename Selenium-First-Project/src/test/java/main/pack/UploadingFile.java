368ca7ae51c1a3ddcf6c7b59350142f980c16bea
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-07 14:09:06 -0400

    upload file using sendkeys

--- Selenium-First-Project/src/test/java/main/pack/UploadingFile.java
+++ Selenium-First-Project/src/test/java/main/pack/UploadingFile.java
@@ -1,5 +1,8 @@
 package main.pack;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -16,6 +19,13 @@ public class UploadingFile {
 		String URL = "https://the-internet.herokuapp.com/upload";
 
 		driver.get(URL);
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		// upload file by using sendkeys method
+		driver.findElement(By.id("file-upload")).sendKeys("C://Users//nazif//Downloads//dummy.docx");
+		// click upload button to upload file to destination
+		driver.findElement(By.xpath("//input[@class='button']")).click();
 
 	}
 
